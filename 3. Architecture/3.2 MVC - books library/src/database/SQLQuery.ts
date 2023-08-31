134a9c5b58556ec8b1f562db5e2dec1c91b7bae5
Andrii Bibik <111527201+Andrii3777@users.noreply.github.com>
2023-08-31 16:22:13 +0300

    Update SQLQuery.ts

--- 3. Architecture/3.2 MVC - books library/src/database/SQLQuery.ts
+++ 3. Architecture/3.2 MVC - books library/src/database/SQLQuery.ts
@@ -1,16 +1,16 @@
-export enum SQLQuery {
-    insertImageQuery = `INSERT INTO image (data) VALUES (?)`,
-    insertBookQuery = `INSERT INTO book (title, pages, year, description, imageId) VALUES (?, ?, ?, ?, ?)`,
-    insertBookAuthorQuery = `INSERT INTO book_author (bookId, authorId) VALUES (?, ?)`,
-    insertAuthorQuery = `INSERT INTO author (name) VALUES (?)`,
-    incrementBookViewsQuery = `UPDATE book SET views = views + 1 WHERE id=?`,
-    incrementBookWantedQuery = `UPDATE book SET wanted = wanted + 1 WHERE id=?`,
-    checkFilledTablesQuery = `SELECT EXISTS (SELECT 1 FROM book_author) AS dataExists`,
-    markBookAsDeletedQuery = `UPDATE book SET deleted = TRUE WHERE id=?`,
-    getImageByIdQuery = `SELECT data FROM image WHERE id = ?`,
-    getLastInsertIdQuery = `SELECT LAST_INSERT_ID() as id`,
-    getAuthorIdByName = `SELECT id FROM author WHERE name = ?`,
-    getAllBooksQuery = `
+export const SQLQuery = {
+    insertImageQuery: `INSERT INTO image (data) VALUES (?)`,
+    insertBookQuery: `INSERT INTO book (title, pages, year, description, imageId) VALUES (?, ?, ?, ?, ?)`,
+    insertBookAuthorQuery: `INSERT INTO book_author (bookId, authorId) VALUES (?, ?)`,
+    insertAuthorQuery: `INSERT INTO author (name) VALUES (?)`,
+    incrementBookViewsQuery: `UPDATE book SET views = views + 1 WHERE id=?`,
+    incrementBookWantedQuery: `UPDATE book SET wanted = wanted + 1 WHERE id=?`,
+    checkFilledTablesQuery: `SELECT EXISTS (SELECT 1 FROM book_author) AS dataExists`,
+    markBookAsDeletedQuery: `UPDATE book SET deleted = TRUE WHERE id=?`,
+    getImageByIdQuery: `SELECT data FROM image WHERE id = ?`,
+    getLastInsertIdQuery: `SELECT LAST_INSERT_ID() as id`,
+    getAuthorIdByName: `SELECT id FROM author WHERE name = ?`,
+    getAllBooksQuery: `
         SELECT
             b.id, b.title, b.pages, b.year, b.views, b.wanted,
             GROUP_CONCAT(a.name SEPARATOR ', ') AS author,
@@ -21,7 +21,7 @@ export enum SQLQuery {
             JOIN author a ON ba.authorId = a.id
         WHERE b.deleted = 0
         GROUP BY b.id;`,
-    getSearchedBooksQuery = `
+    getSearchedBooksQuery: `
         SELECT
         b.id, b.title, b.pages, b.year, b.views, b.wanted,
         GROUP_CONCAT(a.name SEPARATOR ', ') AS author,
@@ -42,7 +42,7 @@ export enum SQLQuery {
         GROUP BY b.id
         LIMIT ?
         OFFSET ?;`,
-    getBookQuery = `
+    getBookQuery: `
         SELECT
             b.id, b.title, b.pages, b.year, GROUP_CONCAT(a.name SEPARATOR ', ') AS author,
             b.description, b.imageId
@@ -52,7 +52,7 @@ export enum SQLQuery {
             JOIN author a ON ba.authorId = a.id
         WHERE b.id = ? AND b.deleted = 0
         GROUP BY b.id;`,
-    getBooksWithLimitOffsetQuery = `
+    getBooksWithLimitOffsetQuery: `
         SELECT
             b.id, b.title, b.pages, b.year, b.views, b.wanted,
             GROUP_CONCAT(a.name SEPARATOR ', ') AS author,
@@ -65,14 +65,14 @@ export enum SQLQuery {
         GROUP BY b.id
         LIMIT ?
         OFFSET ?;`,
-    getBooksNumber = `
+    getBooksNumber: `
         SELECT COUNT(DISTINCT b.id) AS book_count
         FROM
             book b
             JOIN book_author ba ON b.id = ba.bookId
             JOIN author a ON ba.authorId = a.id
             WHERE b.deleted = 0;`,
-    getSearchedBooksNumberQuery = `
+    getSearchedBooksNumberQuery: `
         SELECT COUNT(DISTINCT b.id) AS book_count
         FROM
             book b
