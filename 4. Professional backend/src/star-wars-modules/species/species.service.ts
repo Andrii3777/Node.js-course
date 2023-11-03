35de1734c4e1284c64a46eea51ea2dc1b79d95c8
Andrii Bibik <111527201+Andrii3777@users.noreply.github.com>
2023-11-03 20:45:32 +0300

    Add files via upload

---
+++ 4. Professional backend/src/star-wars-modules/species/species.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateSpeciesDto } from './dto/create-species.dto';
+import { UpdateSpeciesDto } from './dto/update-species.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, Repository } from 'typeorm';
+import { Species } from './entities/species.entity';
+import { Film } from '../films/entities/film.entity';
+import { Person } from '../people/entities/person.entity';
+import { Image } from '../images/entities/image.entity';
+import { UtilService } from '../util.service';
+
+@Injectable()
+export class SpeciesService {
+  constructor(
+    @InjectRepository(Species)
+    private speciesRepository: Repository<Species>,
+    private readonly utilService: UtilService,
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+    @InjectRepository(Film)
+    private readonly filmRepository: Repository<Film>,
+    @InjectRepository(Image)
+    private readonly imageRepository: Repository<Image>,
+  ) { }
+
+  async create(createSpeciesDto: CreateSpeciesDto) {
+    const {
+      peopleIds,
+      filmsIds,
+      imagesIds,
+      ...speciesData
+    } = createSpeciesDto;
+
+    const people = await this.personRepository.findBy({ id: In(peopleIds || []) });
+    const films = await this.filmRepository.findBy({ id: In(filmsIds || []) });
+    const images = await this.imageRepository.findBy({ id: In(imagesIds || []) });
+
+    const species = this.speciesRepository.create({
+      id: await this.utilService.getNextId(this.speciesRepository, 'species'),
+      ...speciesData,
+      people,
+      films,
+      images,
+    });
+
+    return await this.speciesRepository.save(species);
+  }
+
+  async findAll() {
+    return this.speciesRepository.find();
+  }
+
+  async findOne(id: number) {
+    const species = await this.speciesRepository.findOneBy({ id });
+
+    if (!species) throw new NotFoundException(`Species with ID ${id} not found`);
+
+    return species;
+  }
+
+  async update(id: number, updateSpeciesDto: UpdateSpeciesDto) {
+    const species = await this.findOne(id);
+
+    this.utilService.updateFields(species, updateSpeciesDto);
+
+    species.people = await this.utilService.returnEntitiesToUpdate(updateSpeciesDto.peopleIds, this.personRepository);
+    species.films = await this.utilService.returnEntitiesToUpdate(updateSpeciesDto.filmsIds, this.filmRepository);
+    species.images = await this.utilService.returnEntitiesToUpdate(updateSpeciesDto.imagesIds, this.imageRepository);
+
+    return await this.speciesRepository.save(species);
+  }
+
+  async remove(id: number) {
+    return this.speciesRepository.delete(id);
+  }
+}
---
+++ 4. Professional backend/src/star-wars-modules/starships/starships.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateStarshipDto } from './dto/create-starship.dto';
+import { UpdateStarshipDto } from './dto/update-starship.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, In } from 'typeorm';
+import { Starship } from './entities/starship.entity';
+import { Film } from '../films/entities/film.entity';
+import { Person } from '../people/entities/person.entity';
+import { Image } from '../images/entities/image.entity';
+import { UtilService } from '../util.service';
+
+@Injectable()
+export class StarshipsService {
+  constructor(
+    private readonly utilService: UtilService,
+    @InjectRepository(Starship)
+    private starshipRepository: Repository<Starship>,
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+    @InjectRepository(Film)
+    private readonly filmRepository: Repository<Film>,
+    @InjectRepository(Image)
+    private readonly imageRepository: Repository<Image>,
+  ) { }
+
+  async create(createStarshipDto: CreateStarshipDto) {
+    const {
+      pilotsIds,
+      filmsIds,
+      imagesIds,
+      ...starshipData
+    } = createStarshipDto;
+
+    const pilots = await this.personRepository.findBy({ id: In(pilotsIds || []) });
+    const films = await this.filmRepository.findBy({ id: In(filmsIds || []) });
+    const images = await this.imageRepository.findBy({ id: In(imagesIds || []) });
+
+    const starship = this.starshipRepository.create({
+      id: await this.utilService.getNextId(this.starshipRepository, 'starship'),
+      ...starshipData,
+      pilots,
+      films,
+      images,
+    });
+
+    return await this.starshipRepository.save(starship);
+  }
+
+  async findAll() {
+    return this.starshipRepository.find();
+  }
+
+  async findOne(id: number) {
+    const starship = await  this.starshipRepository.findOneBy({ id });
+
+    if (!starship) throw new NotFoundException(`Starship with ID ${id} not found`);
+
+    return starship;
+  }
+
+  async update(id: number, updateStarshipDto: UpdateStarshipDto) {
+    const starship = await this.findOne(id);
+
+    this.utilService.updateFields(starship, updateStarshipDto);
+
+    starship.pilots = await this.utilService.returnEntitiesToUpdate(updateStarshipDto.pilotsIds, this.personRepository);
+    starship.films = await this.utilService.returnEntitiesToUpdate(updateStarshipDto.filmsIds, this.filmRepository);
+    starship.images = await this.utilService.returnEntitiesToUpdate(updateStarshipDto.imagesIds, this.imageRepository);
+
+    return await this.starshipRepository.save(starship);
+  }
+
+  async remove(id: number) {
+    return this.starshipRepository.delete(id);
+  }
+}
