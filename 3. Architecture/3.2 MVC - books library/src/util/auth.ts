f734c5aca83b429082f15e48b0a1d975d43ccc47
Andrii Bibik <111527201+Andrii3777@users.noreply.github.com>
2023-12-29 17:50:51 +0200

    Delete 3. Architecture/3.2 MVC - books library directory

--- 3. Architecture/3.2 MVC - books library/src/util/auth.ts
+++
@@ -1,8 +0,0 @@
-import basicAuth from 'express-basic-auth';
-import env from '../config';
-
-export const auth = basicAuth({
-    users: { 'admin': env.ADMIN_PASSWORD },
-    unauthorizedResponse: 'Unauthorized',
-    challenge: true,
-});
